53d5e57b228d6a98544073de828539ffdcfdd300
preshitaramnani1907 <79746848+preshitaramnani1907@users.noreply.github.com>
2021-02-27 23:11:36 +0530

    Update viewPostLogComponent.js

--- src/components/ViewPostLogComponent/viewPostLogComponent.js
+++ src/components/ViewPostLogComponent/viewPostLogComponent.js
@@ -11,7 +11,7 @@ import Slider from "react-slick"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import { getProgressLog, closeTransaction } from "../../services/post"
-import "./viewPostLogComponent.css"
+import "./ViewPostLogComponent.css"
 
 const titleCase = str => {
   return str
